ff028051267ef45b2bcee28a6eaff9aeef4b65fe
JairZijp <jairzijp123@gmail.com>
2021-10-01 17:05:12 +0200

    WIP create campaign

--- src/force/force.ts
+++ src/force/force.ts
@@ -10,7 +10,8 @@ export class Force {
     this.api = api;
     // TODO: replace this with proper config
     this.config = {
-      FORCE_CONTRACT:"propsonkylin"
+      FORCE_CONTRACT:"propsonkylin",
+      IPFS_NODE: 'https://ipfs.effect.ai'
     }
   }
 
@@ -34,5 +35,78 @@ export class Force {
     const data = await this.api.rpc.get_table_rows(config)
 
     return data;
+  }
+
+  uploadCampaign = async (campaignIpfs: object): Promise<string> => {
+    const blob = new Blob([JSON.stringify(campaignIpfs)], { type: 'text/json' })
+    const formData = new FormData()
+    formData.append('file', blob)
+    if (blob.size > 10000000) {
+      alert('Max file size allowed is 10 MB')
+    } else {
+      try {
+        const response = await fetch(`${this.config.IPFS_NODE}/api/v0/add?pin=true`,
+          {
+            method: 'POST',
+            body: formData
+          })
+        const campaign = await response.json()
+        console.log(campaign)
+        return campaign.Hash
+      } catch (e) {
+        console.log(e)
+        return null
+      }
+    }
+  }
+
+  createCampaign = async (): Promise<object> => {
+    const actions = []
+    
+    // TODO transaction to smart contract
+    // actions.push(
+    //   {
+    //     account: process.env.proposalContract,
+    //     name: 'createprop',
+    //     authorization: [{
+    //       actor: this.wallet.auth.accountName,
+    //       permission: this.wallet.auth.permission
+    //     }],
+    //     data: {
+    //       author: this.wallet.auth.accountName,
+    //       pay: [
+    //         {
+    //           field_0: {
+    //             quantity: Number.parseFloat(this.proposal.reward).toFixed(4) + ' ' + process.env.efxToken,
+    //             contract: process.env.tokenContract
+    //           },
+    //           field_1: payoutTime.toISOString().slice(0, -1)
+    //         }],
+    //       content_hash: this.proposal.content_hash,
+    //       category: parseInt(this.proposal.category),
+    //       cycle: parseInt(this.proposal.cycle),
+    //       transaction_hash: null
+    //     }
+    //   }
+    // )
+
+    try {
+      // do transaction
+      return await this.handleTransaction(actions)
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
+
+  handleTransaction = async (actions): Promise<object> => {
+    try {
+      return await this.api.transact({ actions },
+        {
+          blocksBehind: 3,
+          expireSeconds: 60
+        });
+    } catch (err) {
+      throw new Error(err)
+    }
   }
 }
