2c6ad87e185e95ecab5a2d389d054e8b01a46285
Noman Jabbar <nomanjabbar1999@gmail.com>
2021-10-06 17:47:04 +0200

    move composite key function to sdk

--- src/force/force.ts
+++ src/force/force.ts
@@ -229,5 +229,17 @@ export class Force {
       throw Error(error)
     }       
   }
-
+  /**
+   * Create composite key with `account id` and `campaign id`
+   * @param accountId ID of account logged in
+   * @param campaignId ID of the campaign
+   * @returns uint 64 bit number
+   */
+  getCompositeKey = (accountId: number, campaignId: number) => {
+    const buf = new Serialize.SerialBuffer()
+    buf.reserve(64)
+    buf.pushUint32(accountId)
+    buf.pushUint32(campaignId)
+    return Numeric.binaryToDecimal(buf.getUint8Array(8))
+  }
 }
