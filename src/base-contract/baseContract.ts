5a18ac1612d292916449e346854ea62b93b153fa
JairZijp <jairzijp123@gmail.com>
2021-11-19 16:42:54 +0100

    sendtransaction try catch

--- src/base-contract/baseContract.ts
+++ src/base-contract/baseContract.ts
@@ -177,25 +177,29 @@ export class BaseContract {
    * @returns 
    */
   sendTransaction = async function (owner: string, action: object): Promise<any> {
-    if(isBscAddress(owner)) {
-      // post to relayer
-      return this.fetch(this.config.eos_relayer_url + '/transaction', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(action)
-      }).then(response=>response.json())
-      .then(data=>{ return JSON.parse(data) })
-   } else {
-     return await this.api.transact({
-       actions: [action]
-     }, {
-       blocksBehind: 3,
-       expireSeconds: 30,
-     });
-   }
+    try {
+      if(isBscAddress(owner)) {
+        // post to relayer
+        return this.fetch(this.config.eos_relayer_url + '/transaction', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(action)
+        }).then(response=>response.json())
+        .then(data=>{ return JSON.parse(data) })
+     } else {
+       return await this.api.transact({
+         actions: [action]
+       }, {
+         blocksBehind: 3,
+         expireSeconds: 30,
+       });
+     }
+    } catch (error) {
+      throw new Error(error)
+    }
  }
 
 }
