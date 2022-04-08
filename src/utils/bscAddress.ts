0a05dbe463477d30e676aa4779a06a022d815226
David Britt <david@effect.ai>
2022-04-08 21:46:19 +0200

    Add eos validation util

--- src/utils/bscAddress.ts
+++ src/utils/bscAddress.ts
@@ -6,3 +6,17 @@
 export function isBscAddress(address: string): boolean {
   return (address.length == 42 || address.length == 40)
 }
+
+/**
+ * Utility function: Validate EOS account name
+ * @param eosAccount 
+ * @returns 
+ */
+export function validEosAccount (eosAccount: string): boolean {
+  const validEosChars = 'abcdefghijklmnopqrstuvwxyz12345' // Allowed characters for EOS eosAccount name
+  const maxEosLength = 12
+  const validChars = eosAccount.split('').every((ela: string) => validEosChars.split('').includes((elb: string) => elb === ela))
+  const length = eosAccount.length <= maxEosLength
+  console.debug('valideosaccount', validChars, length)
+  return validChars && length
+}
