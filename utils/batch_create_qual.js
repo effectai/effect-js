939ffcb1565237d649a8fa456dbc7fcc61b13755
djmbritt <david@effect.ai>
2022-07-04 11:58:17 +0200

    map old to new qualis

--- utils/batch_create_qual.js
+++ utils/batch_create_qual.js
@@ -30,7 +30,7 @@ async function main () {
     try {
 
         // Connect to the Effect Network -- testnet / mainnet
-        const sdk = new EffectClient('testnet')
+        const sdk = new EffectClient('mainnet')
 
         // const effectAccount = await connectBscAccount(sdk, configEnv)
         const effectAccount = await connectEosAccount(sdk, configEnv)
@@ -39,65 +39,28 @@ async function main () {
         // Get accountid
         const accountid = sdk.effectAccount.vAccountRows[0].id
 
-        // Loop through the old qualis and create them in the new sy\stem.
-        for (const qual of old_qualis) {
-            // console.log({
-            //     name: qual.name,
-            //     description: qual.description,
-            //     type: null, 
-            //     img_url: 'https://effect.network/img/logo/logo_icon.png',
-            //     is_hidden: false
-            // })
+        // fs read json file
+        const json_file = fs.readFileSync('./qualis.json', 'utf8')
+        const qualis = JSON.parse(json_file)
+        console.log(' qualis', qualis)
 
-            console.log('🔥 Creating qualification: ', qual.name)
+        const map = []
 
-            // For approved qualifications
-            await sdk.force.createQualification(
-                qual.name, 
-                qual.description, 
-                null, 
-                'https://effect.network/img/logo/logo_icon.png',
-                false
-            )
-            .then(console.log)
-            .catch(console.error)
-
-            // For rejected qualifications
-            await sdk.force.createQualification(
-                `${qual.name} - BlockList`,
-                qual.description,
-                null,
-                'https://effect.network/img/logo/logo_icon.png',
-                true
-            )
-            .then(console.log)
-            .catch(console.error)
-
+        for (oldqual of old_qualis) {
+            for (newqual of qualis) {
+                if (oldqual.name === newqual.info.name && oldqual.old_id) {
+                    oldqual.new_id = newqual.id
+                    oldqual.new_name = newqual.info.name
+                    map.push([oldqual.old_id, newqual.id])
+                }
+            }
         }
 
-        console.log('🔥 Done creating qualificaitons: ', old_qualis.length)
-        console.log('🔥 Retrieving newly created qualifications.')
+        console.log(old_qualis)
+        console.log(map)
 
-        // Retrieve all the qualificaitons in order to get the ids.
-        let next = true 
-        let next_key;
-        let qualis = []
-        while ( next ) {
-            const res = await sdk.force.getQualifications(next_key).catch(console.error)
-            qualis = qualis.concat(res.rows)
-            next = res.more
-            next_key = res.next_key
-        }
 
-        // Filter through the qualifications. 
-        console.log(`🔥 Found ${qualis.length} qualifications.`)
-        const ids = qualis.filter(quali => quali.id === accountid)
-        ids.forEach(console.log)
-        console.log(`🔥 Found ${ids.length} qualifications for account with id ${accountid}.`)
 
-        // Save it to a file.
-        fs.writeFileSync('qualis.json', JSON.stringify(ids, null, 2))
-        console.log('🔥 Qualifications saved to qualis.json')
 
         console.log('🔥 Script finished.')
 
@@ -106,3 +69,73 @@ async function main () {
         console.error(e)
     }
 }
+
+
+
+/**
+ * old batch creation page.
+ * 
+        // Loop through the old qualis and create them in the new sy\stem.
+        // for (const qual of example_qualis) {
+        //     // console.log({
+        //     //     name: qual.name,
+        //     //     description: qual.description,
+        //     //     type: null, 
+        //     //     img_url: 'https://effect.network/img/logo/logo_icon.png',
+        //     //     is_hidden: false
+        //     // })
+
+        //     console.log('🔥 Creating qualification: ', qual.name)
+
+        //     // For approved qualifications
+        //     await sdk.force.createQualification(
+        //         qual.name, 
+        //         qual.description, 
+        //         null, 
+        //         'https://effect.network/img/logo/logo_icon.png',
+        //         false
+        //     )
+        //     .then(console.log)
+        //     .catch(console.error)
+
+        //     // For rejected qualifications
+        //     await sdk.force.createQualification(
+        //         `${qual.name} - BlockList`,
+        //         qual.description,
+        //         null,
+        //         'https://effect.network/img/logo/logo_icon.png',
+        //         true
+        //     )
+        //     .then(console.log)
+        //     .catch(console.error)
+
+        console.log('🔥 Done creating qualificaitons: ', example_qualis.length)
+        console.log('🔥 Retrieving newly created qualifications.')
+        // }
+ */
+
+/**
+ * 
+ * retrieve all qualifications from the old system and create them in the new system.
+        // Retrieve all the qualificaitons in order to get the ids.
+        let next = true 
+        let next_key;
+        let qualis = []
+        while ( next ) {
+            const res = await sdk.force.getQualifications(next_key).catch(console.error)
+            console.log(res)
+            qualis = qualis.concat(res.rows)
+            next = res.more
+            next_key = res.next_key
+        }
+
+        // Filter through the qualifications. 
+        console.log(`🔥 Found ${qualis.length} qualifications.`)
+        const ids = qualis.filter(quali => quali.account_id === accountid)
+        ids.forEach(console.log)
+        console.log(`🔥 Found ${ids.length} qualifications for account with id ${accountid}.`)
+
+        // Save it to a file.
+        fs.writeFileSync('qualis.json', JSON.stringify(ids, null, 2))
+        console.log('🔥 Qualifications saved to qualis.json')
+ */
