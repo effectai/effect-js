b27fec5b54dd575fb6a7b94052ccd0850e9e84aa
djmbritt <david@effect.ai>
2023-03-12 12:49:19 +0100

    update campaign interface

--- src/types/campaign.ts
+++ src/types/campaign.ts
@@ -1,13 +1,28 @@
 export interface Campaign {
-  id: number;
-  owner: Array<string>;
+  id: number,
+  owner: Array<string>,
   content: {
-    field_0: number;
-    field_1: string;
-  };
+    field_0: number,
+    field_1: string,
+  },
   reward: {
-    quantity: string;
-    contract: string;
-  };
-  info?: object
+    quantity: string,
+    contract: string,
+  },
+  qualis?: Array<{key: number, value: number}>,
+  info?: {
+    title: string,
+    description: string,
+    instructions: string,
+    template: string,
+    image: string,
+    category: string,
+    example_task: {
+      image_url: string,
+    },
+    version: number,
+    webhook: string,
+    reward: string,
+    estimated_time: string,
+  }
 }
