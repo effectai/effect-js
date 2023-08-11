bde252241e10b12b6b935dd578831888f3895037
David Britt <david@effect.ai>
2023-08-12 01:21:30 +0200

    Supply fetchProvider

--- src/client.ts
+++ src/client.ts
@@ -1,20 +1,24 @@
 import { ClientConfig } from './types/config';
 import { configPresets } from './config';
+import { IpfsService } from './services/ipfs';
 import { TasksService } from './services/tasks';
-import { APIClient, APIClientOptions, FetchProvider } from '@wharfkit/antelope';
+import { APIClient, FetchProvider, FetchProviderOptions } from '@wharfkit/antelope';
 
 export class Client {
     static __classname = 'Client'
 
-    readonly eos: APIClient;
     readonly config: ClientConfig;
+    readonly fetchProvider: FetchProvider;
+    readonly eos!: APIClient;
 
-    constructor (environment: string = 'jungle4') {
+    constructor (environment: string = 'jungle4', fetchConfig?: FetchProviderOptions) {
         this.config = configPresets[environment];
-        this.eos = new APIClient({
-            provider: new FetchProvider(this.config.eosRpcUrl)
+        this.fetchProvider = new FetchProvider(this.config.eosRpcUrl, {
+            fetch: fetchConfig?.fetch || fetch || window.fetch || (() => { throw new Error('No fetch implementation available') })
         });
+        this.eos = new APIClient({ provider: this.fetchProvider });
     }
 
     tasks = new TasksService(this);
+    ipfs = new IpfsService(this);
 }
