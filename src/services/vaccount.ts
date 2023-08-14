dc8fd954ba247bb8944e92129332c0040b4f2c89
Jesse Eisses <jesse.eisses@gmail.com>
2023-08-14 23:44:18 +0200

    Implement vaccount open method

--- src/services/vaccount.ts
+++ src/services/vaccount.ts
@@ -1,4 +1,30 @@
 import { Client } from '../client';
+import {
+    Variant,
+    Struct,
+    UInt8,
+    UInt64,
+    UInt32,
+    Checksum160,
+    Checksum160Type,
+    Asset,
+    Name,
+    NameType
+} from '@wharfkit/antelope';
+
+@Variant.type('vaddress', [Checksum160, Name])
+class VAddress extends Variant {
+    declare value: Checksum160 | Name
+}
+
+@Struct.type('extended_symbol')
+class ExtendedSymbol extends Struct {
+    static abiName = 'extended_symbol'
+    static abiFields = [{name: 'sym', type: 'symbol'},{name: 'contract', type: 'name'}]
+    constructor(sym: Asset.SymbolType, contract: NameType) {
+        super({'sym': sym, 'contract': contract});
+    }
+}
 
 export class VAccountService {
     constructor(private client: Client) {}
@@ -22,5 +48,20 @@ export class VAccountService {
             },
         }
         return this.client.session.transact({ action: transferAction });
+    }
+
+    async open() {
+        const conf = this.client.config;
+        const action = {
+            account: conf.vaccountContract,
+            name: "open",
+            authorization: [this.client.session.permissionLevel],
+            data: {
+                acc: VAddress.from(this.client.session.actor),
+                symbol: new ExtendedSymbol('4,EFX', conf.tokenContract),
+                payer: this.client.session.actor,
+            },
+        }
+        return this.client.session.transact({ action: action });
     }
 };
