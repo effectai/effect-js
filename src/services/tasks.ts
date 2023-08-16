3a12a68e22a7de7d3559d2253de52da2af303674
David Britt <david@effect.ai>
2023-08-17 00:25:14 +0200

    WIP reserveTask

--- src/services/tasks.ts
+++ src/services/tasks.ts
@@ -6,6 +6,10 @@ export class TasksService {
     constructor(private client: Client) {}
 
     // TODO: Keep this one?
+    /**
+     * Retrieve all campaigns published to Effect Network
+     * @returns Campaign[]
+     */
     async getCampaigns (): Promise<Campaign[]> {
         const response = await this.client.eos.v1.chain.get_table_rows({
             code: this.client.config.tasksContract,
@@ -15,7 +19,10 @@ export class TasksService {
         return response.rows
     }
 
-    // Or keep this one?
+    /**
+     * Retrieve all campaigns published to Effect Network
+     * @returns Campaign[]
+     */
     async getAllCampaigns (): Promise<Campaign[]> {
         const rows: Campaign[] = []
         let lowerBound: UInt128 = UInt128.from(0)
@@ -45,6 +52,11 @@ export class TasksService {
         return rows
     }
 
+    /**
+     * 
+     * @param id id of the campaign
+     * @returns Campaign
+     */
     async getCampaign (id: number): Promise<Campaign> {
         const response = await this.client.eos.v1.chain.get_table_rows({
             code: this.client.config.tasksContract,
@@ -60,6 +72,7 @@ export class TasksService {
     }
 
     /**
+     * TODO: add type for reservation
      * GetReservations
      */
     async getReservations (campaignId: number): Promise<any> {
@@ -75,12 +88,39 @@ export class TasksService {
     }
 
     /**
+     * TODO: Add type for user
      * GetMyResercvations for account that is logged in.
-     
      */
+    async getMyReservations (campaignId: number): Promise<any> {
+        const response = await this.client.eos.v1.chain.get_table_rows({
+            code: this.client.config.tasksContract,
+            table: 'reservation',
+            scope: this.client.config.tasksContract,
+            limit: -1,
+            index_position: 3,
+        })
+
+        // TODO check if this account_name is correct
+        response.rows.find((row: any) => row.acccamp === this.client.session.actor)
+
+        const [reservation] = response.rows
+        return reservation
+    }
 
     /**
      * Call reservetask(campaign_id, account_id, quali_assets, payer, sig)
      */
+    async reserveTask (campaignId: number, accountId: number, qualiAssets?: string[]): Promise<any> {
+        const response = await this.client.eos.v1.chain.get_table_rows({
+            code: this.client.config.tasksContract,
+            table: 'account_id',
+            scope: this.client.config.tasksContract,
+            limit: -1,
+            index_position: 'tertiary',
+        })
+
+        const [reservation] = response.rows
+        return reservation
+    }
 
 };
