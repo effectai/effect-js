fa5ab94315e23a4bd66ecff0c1ca8f0d7803d128
David Britt <david@effect.ai>
2023-08-16 12:59:45 +0200

    Add upload method

--- src/services/ipfs.ts
+++ src/services/ipfs.ts
@@ -12,13 +12,43 @@ export enum IpfsContentFormat {
 export class IpfsService {
     constructor(private client: Client) {}
 
+    /**
+     * Upload a file to IPFS
+     * @param {File} file - file to upload to IPFS
+     * @returns {string} - hash of the file uploaded to IPFS
+     */
+    upload = async (obj: object): Promise<string> => {
+        try {
+            const stringified = JSON.stringify(obj)
+            const blob = new Blob([stringified], { type: 'application/json' })
+            const formData = new FormData()
+            formData.append('file', await blob.text())
+
+            if (blob.size > 1024 * 1024 * 10) {
+                throw new Error('File too large, max file size is: 10MB')
+            } else {
+                const response = await this.client.fetchProvider
+                    .fetch(`${this.client.config.ipfsEndpoint}/api/v0/add?pin=true`, {
+                        method: 'POST',
+                        body: formData
+                    })
+                const json = await response.json()
+                return json.hash as string
+            }
+        } catch (error) {
+            console.error(error)
+            throw new Error(error)
+        }
+    }
+
+
   /**
    * Get IPFS Content in JSON, without caching, to be in getIpfsContent. 
    * @param {string} hash - hash of the IPFS content you want to fetch
    * @param {IpfsContentFormat} format - format of the content you are fetching, 
    * @returns content of the ipfs hash in your preferred format
    */
-  fetchIpfsData = async (hash: string, ipfsContentForm: IpfsContentFormat): Promise<any> => {
+  fetch = async (hash: string, ipfsContentForm: IpfsContentFormat): Promise<any> => {
     try {
       let data: { formData: () => any; arrayBuffer: () => any; blob: () => any; text: () => any; json: () => any; };
 
