2d56942fcbce774b28e1f395b605591dd9e22b85
David Britt <david@effect.ai>
2023-08-22 13:11:36 +0200

    Add utility script

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+// Using es6 import
+// (async () => {
+//     const efxSdk = await import('@effectai/effect-js')
+//     console.log(efxSdk)
+// })()
+
+
+// Using require
+// const efxSdk = require('@effectai/effect-js')
+// console.log(efxSdk)
