975ac041128a1a05f5a201fa3bc390fca54226ff
David Britt <david@effect.ai>
2023-09-04 15:09:41 +0200

    seperate get task from campaign

--- test/tasks.spec.ts
+++ test/tasks.spec.ts
@@ -32,7 +32,6 @@ describe('Tasks', async () => {
         expect(owner).toEqual('efxefxefxefx')
     })
 
-
     test('Get first Campaign', async () => {
         const campaign = await client.tasks.getCampaign(0)
 
@@ -47,20 +46,17 @@ describe('Tasks', async () => {
         const batch = await client.tasks.getBatch(0)
         expect(batch).toBeDefined()
         expect(batch.id).toEqual(0)
-        
-        const taskData = await client.tasks.getTaskData(batch.id)
-        expect(taskData).toBeDefined()
-        expect(taskData).toHaveLength(1)
-
     })
 
     test('User login', async () => {
 
+        // Test that the client throws an error if we try to reserve a task without logging in.
         expect(() => client.requireSession()).toThrowError()
 
+        // Login, make sure these are set in your .env.test file.
         client.login(process.env.VITE_EOSACC!, process.env.VITE_EOSPERM!, process.env.VITE_EOSPK!)
-
         expect(client.session).toBeDefined()
+        expect(() => client.requireSession()).not.toThrowError()
     })
 
     test('Reserve Task', async () => {
@@ -75,6 +71,14 @@ describe('Tasks', async () => {
         expect(reservation.campaign_id).toEqual(0)
         
     })
+
+    test('Get Task from Campaign', async () => {
+        const campaign = await client.tasks.getCampaign(0)
+        const reservation = await client.tasks.reserveTask(campaign.id)
+        const taskData = await client.tasks.getTaskData(reservation)
+        expect(taskData).toBeDefined()
+    })
+
 
 
 })
