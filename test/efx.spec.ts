26849a25c1753ebbb20cecf79058605ed103c727
David Britt <david@effect.ai>
2023-09-04 15:11:18 +0200

    add efx tests

---
+++ test/efx.spec.ts
@@ -0,0 +1,19 @@
+import { TokenService } from './../src/services/efx';
+import { Client } from './../src/client';
+
+import { expect, test, describe } from 'vitest'
+
+const client = new Client('eos')
+
+describe('EfxService', () => {
+
+    test('should return the price of EFX', async () => {
+        const efxPrice = await client.efx.getEfxPrice()
+        expect(efxPrice).toBeDefined()
+        expect(efxPrice).not.toBeNull()
+        expect(typeof efxPrice).toBe('number')
+        expect(efxPrice).toBeGreaterThan(0)
+
+    })
+
+})
