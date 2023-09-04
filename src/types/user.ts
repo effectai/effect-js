44cda272badd392ebad1c87e1b4c27f08aa05c8e
David Britt <david@effect.ai>
2023-09-04 15:08:07 +0200

    add defibox pair interface

--- src/types/user.ts
+++ src/types/user.ts
@@ -13,3 +13,23 @@ export interface efxTicker {
     eth: number,
     usd: number
 }
+
+export interface DefiBoxPair {
+    id: number;
+    token0: {
+      contract: string;
+      symbol: string;
+    };
+    token1: {
+      contract: string;
+      symbol: string;
+    };
+    reserve0: string;
+    reserve1: string;
+    liquidity_token: number;
+    price0_last: string;
+    price1_last: string;
+    price0_cumulative_last: string;
+    price1_cumulative_last: number;
+    block_time_last: string;
+  }
