927ab9def95d1de365ca6e1e30cc43c80a6d2c22
David Britt <david@effect.ai>
2023-09-06 15:58:17 +0200

    Lorem test for ipfs

--- test/ipfs.spec.ts
+++ test/ipfs.spec.ts
@@ -1,13 +1,16 @@
 // import { IpfsContentFormat, IpfsService } from '../src/services/ipfs'
-// import { Client } from '../src/client'
+import { Client } from '../src/client'
+import { expect, test } from 'vitest'
 
+const client = new Client('jungle4')
 
-// import { expect, test } from 'vitest'
+const hash = 'QmXKn3tGx6CoyaMVgR9L6df3Dtbocx2zvMoQxSNNU1oEnV'
 
-// const client = new Client('jungle4')
-
-// const hash = 'QmXKn3tGx6CoyaMVgR9L6df3Dtbocx2zvMoQxSNNU1oEnV'
-
+test('Should check that client is defined', async () => {
+    expect(client).toBeDefined()
+    expect(client).not.toBeNull()
+    expect(client).toBeInstanceOf(Client)
+})
 
 // test('should upload ipfs info with formdata web-std', async () => {
 //     const helloWorldHash = 'QmNrEidQrAbxx3FzxNt9E6qjEDZrtvzxUVh47BXm55Zuen'
