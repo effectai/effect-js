f7b49eeacf375d59fc79e38ef12305ac4b7d19b0
David Britt <david@effect.ai>
2023-09-07 23:47:22 +0200

    lint client

--- src/client.ts
+++ src/client.ts
@@ -1,58 +1,58 @@
-import { ClientConfig } from './types/config';
-import { configPresets } from './config';
-import { IpfsService } from './services/ipfs';
-import { TasksService } from './services/tasks';
-import { VAccountService } from './services/vaccount';
-import { TokenService } from './services/token';
+import { type ClientConfig } from './types/config'
+import { configPresets } from './config'
+import { IpfsService } from './services/ipfs'
+import { TasksService } from './services/tasks'
+import { VAccountService } from './services/vaccount'
+import { TokenService } from './services/token'
 
-import { APIClient, FetchProvider } from '@wharfkit/antelope';
-import { Session } from "@wharfkit/session"
-import { WalletPluginPrivateKey } from "@wharfkit/wallet-plugin-privatekey"
+import { APIClient, FetchProvider } from '@wharfkit/antelope'
+import { Session } from '@wharfkit/session'
+import { WalletPluginPrivateKey } from '@wharfkit/wallet-plugin-privatekey'
 
 export class Client {
     static __classname = 'Client'
 
-    readonly config: ClientConfig;
-    readonly fetchProvider: FetchProvider;
-    readonly eos!: APIClient;
-    session!: Session;
+    readonly config: ClientConfig
+    readonly fetchProvider: FetchProvider
+    readonly eos!: APIClient
+    session!: Session
 
     /**
      * Create a new Effect Network Client instance
      * @param {string} environment Which network you would like to connect to, defaults to 'jungle4'
      */
     constructor (environment: string = 'jungle4') {
-        this.config = configPresets[environment];
-        this.fetchProvider = new FetchProvider(this.config.eosRpcUrl, { 
-            fetch : fetch || window.fetch
-         });
-        this.eos = new APIClient({ provider: this.fetchProvider });
+        this.config = configPresets[environment]
+        this.fetchProvider = new FetchProvider(this.config.eosRpcUrl, {
+            fetch: fetch ?? window.fetch
+        })
+        this.eos = new APIClient({ provider: this.fetchProvider })
     }
 
-    tasks = new TasksService(this);
-    ipfs = new IpfsService(this);
-    vaccount = new VAccountService(this);
-    efx = new TokenService(this);
+    tasks = new TasksService(this)
+    ipfs = new IpfsService(this)
+    vaccount = new VAccountService(this)
+    efx = new TokenService(this)
 
-    login (actor: string, permission: string, privateKey: string) {
-        const walletPlugin = new WalletPluginPrivateKey(privateKey);
+    login (actor: string, permission: string, privateKey: string): void {
+        const walletPlugin = new WalletPluginPrivateKey(privateKey)
         this.session = new Session({
-            actor: actor,
-            permission: permission,
+            actor,
+            permission,
             chain: {
                 id: this.config.eosChainId,
-                url: this.config.eosRpcUrl,
+                url: this.config.eosRpcUrl
             },
-            walletPlugin,
-        });
+            walletPlugin
+        })
     }
 
     /**
      * Require a session to be set (make sure user is logged in), otherwise throw an error.
      */
     requireSession (): void {
-        if (!this.session) {
-            throw new Error('Session is required for this method, please login.');
+        if (this.session === undefined || this.session === null) {
+            throw new Error('Session is required for this method, please login.')
         }
     }
 }
