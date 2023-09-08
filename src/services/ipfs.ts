9394d71ff2759dd819bae744cc292c4026ef2dd8
Jesse Eisses <jesse.eisses@gmail.com>
2023-09-08 11:19:31 +0200

    Fix IpfsService: use internal Blob and update code formatting

--- src/services/ipfs.ts
+++ src/services/ipfs.ts
@@ -1,11 +1,11 @@
-import { type Client } from '../client'
+import { type Client } from '../client';
 
 export enum IpfsContentFormat {
     FormData = 'formdata',
     ArrayBuffer = 'arraybuffer',
     Blob = 'blob',
     Text = 'text',
-    JSON = 'json'
+    JSON = 'json',
 }
 
 export class IpfsService {
@@ -13,70 +13,74 @@ export class IpfsService {
 
     upload = async (obj: object): Promise<any> => {
         try {
-            const stringified = JSON.stringify(obj)
+            let blob = new Blob([JSON.stringify(obj)], { type: 'application/json' });
 
-            let blob: any
-            if (Blob !== undefined || window.Blob !== undefined) {
-                blob = new Blob([stringified], { type: 'application/json' })
-            } else {
-                const Blob = (await import('node:buffer')).Blob
-                blob = new Blob([stringified], { type: 'application/json' })
-            }
-
-            const formData = new FormData()
-            formData.append('file', blob)
+            const formData = new FormData();
+            formData.append('file', blob);
 
             if (blob.size > 1024 * 1024 * 10) {
-                throw new Error('File too large, max file size is: 10MB')
+                throw new Error('File too large, max file size is: 10MB');
             } else {
                 const requestOptions: RequestInit = {
                     method: 'POST',
-                    body: formData
-                }
+                    body: formData,
+                };
 
-                const response = await this.client.fetchProvider
-                    .fetch(`${this.client.config.ipfsEndpoint}/api/v0/add?pin=true`, requestOptions)
+                const response = await this.client.fetchProvider.fetch(
+                    `${this.client.config.ipfsEndpoint}/api/v0/add?pin=true`,
+                    requestOptions
+                );
 
                 if (!response.ok) {
-                    const errorText = await response.text()
-                    console.error(errorText)
-                    throw new Error('Error uploading file to IPFS')
+                    const errorText = await response.text();
+                    throw new Error(`Error in IPFS upload: ${errorText}`);
                 } else {
-                    const json = await response.json()
-                    return json.Hash as string
+                    const json = await response.json();
+                    return json.Hash as string;
                 }
             }
         } catch (error) {
-            console.error(error)
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 
     /**
-   * Get IPFS Content in JSON, without caching, to be in getIpfsContent.
-   * @param {string} hash - hash of the IPFS content you want to fetch
-   * @param {IpfsContentFormat} format - format of the content you are fetching,
-   * @returns content of the ipfs hash in your preferred format
-   */
-    fetch = async (hash: string, ipfsContentForm: IpfsContentFormat = IpfsContentFormat.JSON): Promise<any> => {
+     * Get IPFS Content in JSON, without caching, to be in getIpfsContent.
+     * @param {string} hash - hash of the IPFS content you want to fetch
+     * @param {IpfsContentFormat} format - format of the content you are fetching,
+     * @returns content of the ipfs hash in your preferred format
+     */
+    fetch = async (
+        hash: string,
+        ipfsContentForm: IpfsContentFormat = IpfsContentFormat.JSON
+    ): Promise<any> => {
         try {
-            let data: { formData: () => any, arrayBuffer: () => any, blob: () => any, text: () => any, json: () => any }
-            data = await this.client.fetchProvider.fetch(`${this.client.config.ipfsEndpoint}/ipfs/${hash}`)
+            let data: {
+                formData: () => any,
+                arrayBuffer: () => any,
+                blob: () => any,
+                text: () => any,
+                json: () => any
+            };
+
+            data = await this.client.fetchProvider.fetch(
+                `${this.client.config.ipfsEndpoint}/ipfs/${hash}`
+            );
 
             // Return the IPFS content in the format you want
             switch (ipfsContentForm) {
                 case IpfsContentFormat.FormData:
-                    return data.formData()
+                    return data.formData();
                 case IpfsContentFormat.ArrayBuffer:
-                    return data.arrayBuffer()
+                    return data.arrayBuffer();
                 case IpfsContentFormat.Blob:
-                    return data.blob()
+                    return data.blob();
                 case IpfsContentFormat.Text:
-                    return data.text()
+                    return data.text();
                 case IpfsContentFormat.JSON:
-                    return await data.json()
+                    return await data.json();
                 default:
-                    return data
+                    return data;
             }
         } catch (error) {
             console.error(error)
