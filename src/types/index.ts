9499dd0db0c686ab71747d18534e10823cfdf209
David Britt <david@effect.ai>
2023-09-13 16:54:54 +0200

    index types

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+import { type Campaign, type Batch, type Reservation, type TasksSettings } from './campaign'
+import { type ClientConfig } from './config'
+import { type DefiBoxPair, type VAccount, type efxTicker } from './user'
+
+export type {
+    ClientConfig,
+    DefiBoxPair,
+    VAccount,
+    efxTicker,
+    Campaign,
+    Batch,
+    Reservation,
+    TasksSettings
+}
