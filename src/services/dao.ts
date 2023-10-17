66bd3d1f22b3adba1bb69f7201676555aca11344
David Britt <david@effect.ai>
2023-10-17 23:32:16 +0200

    Add DaoService class

---
+++ src/services/dao.ts
@@ -0,0 +1,23 @@
+import { type Client } from "../client";
+import { DaoConfig } from "../types/campaign";
+
+export class DaoService {
+    constructor (private readonly client: Client) {}
+
+    getDaoConfig = async (): Promise<DaoConfig> => {
+        try {
+            const { rows } = await this.client.eos.v1.chain.get_table_rows({
+                code: this.client.config.daoContract,
+                scope: this.client.config.daoContract,
+                table: 'config',
+                limit: 1
+            })
+            const [ config ] = rows
+            return config
+        } catch (error) {
+            console.error(error)
+            throw new Error(error)
+        }
+    }
+
+}
