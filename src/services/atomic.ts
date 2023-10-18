a87660285dd3e758738734c1b389054ba0550d7a
David Britt <david@effect.ai>
2023-10-18 14:48:48 +0200

    Add getAccountAsssets and getAssets methods

--- src/services/atomic.ts
+++ src/services/atomic.ts
@@ -1,18 +1,46 @@
+import { UInt128 } from "@wharfkit/antelope";
 import { type Client } from "../client";
+import { AtomicAsset } from "../types/campaign";
 
+/**
+ * AtomicAssetsService
+ * 
+ * This service is used to interact with the atomicassets contract.
+ *
+ * Read more on the atomicassets contracts here:
+ * https://github.com/pinknetworkx/atomicassets-contract/wiki/
+ */
 export class AtomicAssetsService {
     constructor (private readonly client: Client) {}
 
-    getAtomicAssets = async (): Promise<any> => {
-        // TODO: Retrieve atomic assets
+    /**
+     * Retrieve atomic assets from the atomicassets contract 
+     * @returns {Promise<any>} Returns the atomic assets config
+     */
+    getAccountAssets = async (account: string): Promise<AtomicAsset[]> => {
         const { rows } = await this.client.eos.v1.chain.get_table_rows({
             code: this.client.config.atomicAssetsContract,
-            scope: this.client.config.atomicAssetsContract,
-            table: 'config',
-            limit: 1
+            scope: account,
+            table: 'assets',
+            limit: 100
         })
-        const [ config ] = rows
-        return config
+        return rows
     }
 
+    /**
+     * 
+     * @param assetId 
+     */
+    getAsset = async (owner: string, assetId: string): Promise<AtomicAsset> => {
+        const { rows } = await this.client.eos.v1.chain.get_table_rows({
+            code: this.client.config.atomicAssetsContract,
+            scope: owner,
+            table: 'assets',
+            limit: 100,
+            lower_bound: UInt128.from(assetId),
+            upper_bound: UInt128.from(assetId)
+        })
+        const [ asset ] = rows
+        return asset
+    }
 }
