03231043b6260ce43dbb1448b07b6b71dd309ed2
David Britt <david@effect.ai>
2024-04-22 21:25:35 -0400

    Fix getPrice method
    Check for window.fetch and fetch

--- docs/sidebar.ts
+++ docs/sidebar.ts
@@ -1,31 +1,48 @@
 import type { Sidebar } from "vocs";
 
 export const sidebar = {
-	"/docs/": [
-		{
-			text: "Introduction",
-			items: [
-				{ text: "Why Effect AI", link: "/docs/introduction" },
-				{ text: "Getting Started", link: "/docs/getting-started" },
-				{ text: "Local Development", link: "/docs/local-development" },
-			],
-		},
-		{
-			text: "Guides",
-			items: [
-				{
-					text: "Create Your First Campaign",
-					link: "/docs/guides/create-a-campaign",
-				},
-			],
-		},
-		{
-			text: "Token",
-			items: [{ text: "getPrice", link: "/docs/token/getPrice" }],
-		},
-		{
-			text: "Tasks",
-			items: [{ text: "getCampaigns", link: "/docs/tasks/getCampaigns" }],
-		},
-	],
+  "/docs/": [
+    {
+      text: "Introduction",
+      items: [
+        { text: "Why Effect AI", link: "/docs/introduction" },
+        { text: "Getting Started", link: "/docs/gettingStarted" },
+        { text: "Local Development", link: "/docs/localDevelopment" },
+      ],
+    },
+    {
+      text: "Guides",
+      items: [
+        {
+          text: "Create Your First Campaign",
+          link: "/docs/guides/createACampaign",
+        },
+      ],
+    },
+    {
+      text: "Token",
+      items: [{ text: "getPrice", link: "/docs/token/getPrice" }],
+    },
+    {
+      text: "Tasks",
+      items: [{ text: "getCampaigns", link: "/docs/tasks/campaigns/getCampaigns" }],
+    },
+    {
+      text: "Local Development",
+      items: [{ text: "localDevelopment", link: "/docs/localDevelopment" }],
+    },
+    {
+      text: "Glossary",
+      items: [
+        { text: "Client Options", link: "/docs/glossary/clientOptions" }
+      ]
+    },
+    {
+      text: "FAQ",
+      items: [
+        { text: "FAQ", link: "/docs/faq" }
+      ]
+    }
+
+  ],
 } as const satisfies Sidebar;
