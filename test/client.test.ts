2a212a2864dfd2a4f333f739f259c454181ec12e
David Britt <david@effect.ai>
2024-04-23 23:03:41 -0400

    test client setup

---
+++ test/client.test.ts
@@ -0,0 +1,20 @@
+import { expect, test, describe, beforeAll } from "bun:test";
+import { testClient } from "./helpers";
+import { Client } from "../src/client";
+
+describe("Client SDK", () => {
+  let client: Client;
+  beforeAll(async () => {
+    client = await testClient();
+  });
+
+  test("Client defined", () => {
+    expect(client).toBeDefined();
+  });
+
+  test("Client session connected", async () => {
+    expect(client.session).toBeDefined();
+    expect(client.session?.vAccount).toBeDefined();
+    expect(client.session?.actor.toString()).toEqual("forcedev1234");
+  });
+});
