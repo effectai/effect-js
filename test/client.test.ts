322eaf614540ca5d406b39fe7d8d27b2dda8a2c0
David Britt <david@effect.ai>
2024-04-24 00:01:28 -0400

    Add getVAccounts test

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,20 +1,29 @@
 import { expect, test, describe, beforeAll } from "bun:test";
 import { testClient } from "./helpers";
 import { Client } from "../src/client";
+import { getVAccounts, getAccountById } from "../src/exports";
+import { Name } from "@wharfkit/antelope";
 
 describe("Client SDK", () => {
-  let client: Client;
-  beforeAll(async () => {
-    client = await testClient();
-  });
+	let client: Client;
+	beforeAll(async () => {
+		client = await testClient();
+	});
 
-  test("Client defined", () => {
-    expect(client).toBeDefined();
-  });
+	test("Client defined", () => {
+		expect(client).toBeDefined();
+	});
 
-  test("Client session connected", async () => {
-    expect(client.session).toBeDefined();
-    expect(client.session?.vAccount).toBeDefined();
-    expect(client.session?.actor.toString()).toEqual("forcedev1234");
-  });
+	test("Client session connected", async () => {
+		expect(client.session).toBeDefined();
+		expect(client.session?.vAccount).toBeDefined();
+		expect(client.session?.actor.toString()).toEqual("forcedev1234");
+	});
+
+	test("getVAccounts", async () => {
+		const actor = Name.from("forcedev1234");
+		const [vacc] = await getVAccounts({ client, actor });
+		expect(vacc.address[1]).toEqual("forcedev1234");
+		expect(vacc.id).toBeNumber();
+	});
 });
