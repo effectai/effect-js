486efdd3cc8c05f032bb2b31459f6adb08c60a43
David Britt <david@effect.ai>
2024-04-23 23:03:27 -0400

    Set up create test client helper

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,26 +1,29 @@
 import { Session } from "@wharfkit/session";
 import { WalletPluginPrivateKey } from "@wharfkit/wallet-plugin-privatekey";
-import { jungle4 } from "../src/constants/network";
-import { createClient } from "./../src/client";
+import { jungle4, eos } from "../src/exports";
+import { Client, createClient } from "./../src/client";
 
-const client = createClient(jungle4, {});
+export const testClient = async (): Promise<Client> => {
+  const client = createClient({ network: jungle4 });
 
-//jungle 4 test key
-const walletPlugin = new WalletPluginPrivateKey(
-	"5KSG1pLHubiQ2JD4G4Pr32zxz7oQvpagBofYPrdS1FALKVjxdPM",
-);
+  //jungle 4 test key
+  const walletPlugin = new WalletPluginPrivateKey(
+    "5KSG1pLHubiQ2JD4G4Pr32zxz7oQvpagBofYPrdS1FALKVjxdPM",
+  );
 
-//set a test session to jungle
-client.state.setState({
-	session: new Session({
-		actor: "forcedev1234",
-		permission: "active",
-		chain: {
-			id: "73e4385a2708e6d7048834fbc1079f2fabb17b3c125b146af438971e90716c4d",
-			url: "https://jungle4.greymass.com",
-		},
-		walletPlugin,
-	}),
-});
+  // Add Test session
+  const session = new Session({
+    actor: "forcedev1234",
+    permission: "active",
+    chain: {
+      id: "73e4385a2708e6d7048834fbc1079f2fabb17b3c125b146af438971e90716c4d",
+      url: "https://jungle4.greymass.com",
+    },
+    walletPlugin,
+  });
 
-export { client };
+  // Set up test session
+  await client.setSession(session);
+
+  return client;
+};
