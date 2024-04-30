3aa8ba2421105dc9fdf53fa872d93e2887c19965
David Britt <david@effect.ai>
2024-04-30 15:04:00 -0400

    Use jungle for deposit test

--- src/actions/vaccount/deposit.test.ts
+++ src/actions/vaccount/deposit.test.ts
@@ -1,19 +1,23 @@
 import { describe, test, expect } from "bun:test";
 import { deposit } from "./deposit";
 import { destructureEnv, testClientSession } from "../../testHelper";
-import { eos } from "../../exports";
+import { eos, jungle4 } from "../../exports";
 import { getOrCreateVAccount } from "./getOrCreate";
 import { Name } from "@wharfkit/antelope";
 
 describe("deposit", async () => {
-	test("Check that deposit is functioning correctly", async () => {
-		const { network, actor } = destructureEnv(eos);
+	test.todo("Should throw an error when Session is not set on Client.", () => {
+		// TODO: implement test
+	});
+
+	test.skip("Check that deposit is functioning correctly", async () => {
+		const { network, actor } = destructureEnv(jungle4);
 		const client = await testClientSession({ testEnvNetwork: network });
 		console.debug(client.network);
 		const acc = Name.from(actor);
 		const vAccount = await getOrCreateVAccount({ client, actor: acc });
 		const vAccId = Number(vAccount.id);
-		const result = await deposit({ client, vAccountId: vAccId, amount: 1 });
+		const result = await deposit({ client, vAccountId: vAccId, amount: 0.1 });
 		console.debug(result);
 	});
 });
