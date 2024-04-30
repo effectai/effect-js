65a733a633324bd6cad8d01d290af309818b2379
David Britt <david@effect.ai>
2024-04-30 15:11:07 -0400

    Use swap update from Jeff

--- src/actions/token/swap.ts
+++ src/actions/token/swap.ts
@@ -1,100 +1,92 @@
-import { type AnyAction, Asset, type Name } from "@wharfkit/antelope";
+import {
+	type AnyAction,
+	Asset,
+	type Name,
+	PermissionLevelType,
+} from "@wharfkit/antelope";
 import type { Client } from "../../client";
 import { DefiBoxPairEnum } from "./getDefiBoxPair";
 import { getPrice } from "./getPrice";
-import type { TransactResult } from "@wharfkit/session";
-
-export enum swapDirectionEnum {
-  EfxToUsdt = "efxToUsdt",
-  UsdtToEfx = "usdtToEfx",
-}
 
 export const swapDirection = {
-  EfxToUsdt: `${DefiBoxPairEnum.EosEfx}-${DefiBoxPairEnum.EosUsdt}`,
-  UsdtToEfx: `${DefiBoxPairEnum.EosUsdt}-${DefiBoxPairEnum.EosEfx}`,
+	EfxToUsdt: `${DefiBoxPairEnum.EosEfx}-${DefiBoxPairEnum.EosUsdt}`,
+	UsdtToEfx: `${DefiBoxPairEnum.EosUsdt}-${DefiBoxPairEnum.EosEfx}`,
 };
 
-export type BuildSwapActionArgs = {
-  direction: swapDirectionEnum;
-  actor: Name;
-  authorization: { permission: Name; actor: Name }[];
-  amount: number;
-  efxPrice: number;
+export const buildSwapAction = (
+	direction: string,
+	actor: Name,
+	authorization: PermissionLevelType[],
+	amount: number,
+	efxPrice: number,
+) => {
+	if (!authorization || !authorization.length || !actor) {
+		throw new Error("No authorization provided");
+	}
+
+	if (!amount || !efxPrice) {
+		throw new Error("Amount or EFX price not provided");
+	}
+
+	const valueAmount = efxPrice * amount;
+
+	const swapAction: {
+		[key: string]: AnyAction;
+	} = {
+		[swapDirection.EfxToUsdt]: {
+			account: "effecttokens",
+			name: "transfer",
+			authorization,
+			data: {
+				from: actor,
+				to: "swap.defi",
+				quantity: Asset.from(amount, "4,EFX"),
+				memo: `swap,${valueAmount},${swapDirection.EfxToUsdt}`,
+			},
+		},
+		[swapDirection.UsdtToEfx]: {
+			account: "tethertether",
+			name: "transfer",
+			authorization,
+			data: {
+				from: actor,
+				to: "swap.defi",
+				quantity: Asset.from(amount, "4,USDT"),
+				memo: `swap,${valueAmount},${swapDirection.UsdtToEfx}`,
+			},
+		},
+	};
+
+	return swapAction[direction];
 };
 
-export const buildSwapAction = ({
-  direction,
-  actor,
-  authorization,
-  amount,
-  efxPrice,
-}: BuildSwapActionArgs): AnyAction => {
-  if (!authorization || !authorization.length || !actor) {
-    throw new Error("No authorization provided");
-  }
+export const swap = async (
+	client: Client,
+	amount: number,
+	direction: string,
+) => {
+	try {
+		if (!client.session) {
+			throw new Error("Session is required for this method.");
+		}
 
-  if (!amount || !efxPrice) {
-    throw new Error("Amount or EFX price not provided");
-  }
+		const { transact, wharfKitSession, actor, authorization } = client.session;
+		const efxPrice = await getPrice();
 
-  const valueAmount = efxPrice * amount;
+		const action = buildSwapAction(
+			direction,
+			actor,
+			authorization,
+			amount,
+			efxPrice,
+		);
 
-  const swapAction: { [key: string]: AnyAction } = {
-    efxToUsdt: {
-      account: "effecttokens",
-      name: "transfer",
-      authorization,
-      data: {
-        from: actor,
-        to: "swap.defi",
-        quantity: Asset.from(amount, "4,EFX"),
-        memo: `swap,${valueAmount},${swapDirection.EfxToUsdt}`,
-      },
-    },
-    usdtToEfx: {
-      account: "tethertether",
-      name: "transfer",
-      authorization,
-      data: {
-        from: actor,
-        to: "swap.defi",
-        quantity: Asset.from(amount, "4,USDT"),
-        memo: `swap,${valueAmount},${swapDirection.UsdtToEfx}`,
-      },
-    },
-  };
+		if (!action) {
+			throw new Error("Invalid swap action");
+		}
 
-  return swapAction[direction];
-};
-
-export type SwapArgs = {
-  client: Client;
-  amount: number;
-  direction: swapDirectionEnum;
-};
-
-export const swap = async ({
-  client,
-  amount,
-  direction,
-}: SwapArgs): Promise<TransactResult> => {
-  if (!client.session) {
-    throw new Error("Swap: Session is required for this method.");
-  }
-  try {
-    const { transact, actor, authorization } = client.session;
-    const efxPrice = await getPrice();
-
-    const action = buildSwapAction({
-      direction,
-      actor,
-      authorization,
-      amount,
-      efxPrice,
-    });
-
-    return await transact({ action });
-  } catch (e) {
-    throw new Error(`Error swapping tokens\n${e}`);
-  }
+		return await transact({ action });
+	} catch (e) {
+		throw new Error(`Error swapping: ${e}`);
+	}
 };
