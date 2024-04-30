ddca5be34851304e8e0b4ea298a24d52e1814017
David Britt <david@effect.ai>
2024-04-30 11:51:12 -0400

    Add docs for getBalance

--- src/actions/token/getBalance.ts
+++ src/actions/token/getBalance.ts
@@ -1,4 +1,4 @@
-import type { Name } from "@wharfkit/session";
+import type { Asset, Name } from "@wharfkit/session";
 import type { Client } from "../../exports";
 
 export type GetBalanceArgs = {
@@ -6,7 +6,23 @@ export type GetBalanceArgs = {
 	actor: Name;
 };
 
-export const getBalance = async ({ client, actor }: GetBalanceArgs) => {
+/**
+ * Get the balance of a user
+ * @param {{ client: Client, actor: Name}} Effect SDK client and the actor to get the balance for.
+ * @returns {Promise<Asset>} The balance of the user
+ * @throws {Error} if no balance is found
+ *
+ * @example
+ * const client = createClient({ network: eos });
+ * const actor = Name.from("cryptonode42");
+ * const balance = await getBalance({ client, actor });
+ * console.log(balance.toString());
+ * // => "100.0000 EFX"
+ */
+export const getBalance = async ({
+	client,
+	actor,
+}: GetBalanceArgs): Promise<Asset> => {
 	const { network, provider } = client;
 	const { contracts } = network.config.efx;
 
