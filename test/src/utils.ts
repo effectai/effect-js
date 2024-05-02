66c4efa68bc5426c27dbabfedc4016197a858054
David Britt <david@effect.ai>
2024-05-02 19:48:11 -0400

    Add permissionLevel and fix env vars names

--- test/src/utils.ts
+++ test/src/utils.ts
@@ -1,8 +1,8 @@
 import {
-	PrivateKey,
-	type PrivateKeyType,
-	Session,
-	PermissionLevel,
+  PrivateKey,
+  type PrivateKeyType,
+  Session,
+  PermissionLevel,
 } from "@wharfkit/session";
 import { WalletPluginPrivateKey } from "@wharfkit/wallet-plugin-privatekey";
 import { jungle4, eos } from "../../src/exports";
@@ -11,71 +11,58 @@ import type { Client } from "../../src/client";
 import type { Network } from "../../src/types/network";
 
 declare module "bun" {
-	interface Env {
-		TESTNET_PERMISSION: string;
-		TESTNET_NETWORK_NAME: string;
-		TESTNET_ACTOR: string;
-		TESTNET_PRIVATE_KEY: string;
-		MAINNET_PERMISSION: string;
-		MAINNET_NETWORK_NAME: string;
-		MAINNET_ACTOR: string;
-		MAINNET_PRIVATE_KEY: string;
-	}
+  interface Env {
+    TESTNET_PERMISSION: string;
+    TESTNET_NETWORK_NAME: string;
+    TESTNET_ACTOR: string;
+    TESTNET_PRIVATE_KEY: string;
+    MAINNET_PERMISSION: string;
+    MAINNET_NETWORK_NAME: string;
+    MAINNET_ACTOR: string;
+    MAINNET_PRIVATE_KEY: string;
+  }
 }
 
 export interface testEnv {
-	network: Network;
-	networkName: string;
-	permission: string;
-	actor: string;
-	privateKey: PrivateKeyType;
+  network: Network;
+  networkName: string;
+  permission: string;
+  actor: string;
+  privateKey: PrivateKeyType;
 }
 
-export const destructureEnv = (networkEnv: Network) => {
-	return {
-		network: jungle4,
-		networkName: process.env.NETWORK_NAME,
-		permission: process.env.PERMISSION!,
-		actor: process.env.ACTOR!,
-		privateKey: PrivateKey.from(process.env.PRIVATE_KEY!),
-	};
+export const destructureEnv = (network: Network) => {
+  return {
+    network: jungle4,
+    networkName: process.env.TESTNET_NETWORK_NAME,
+    permission: process.env.TESTNET_PERMISSION!,
+    actor: process.env.TESTNET_ACTOR!,
+    privateKey: PrivateKey.from(process.env.TESTNET_PRIVATE_KEY!),
+  };
 };
 
-export const testClientSession = async ({
-	testEnvNetwork,
-}: { testEnvNetwork: Network }): Promise<Client> => {
-	// Retrieve parameters for session.
-	const { network, permission, actor, privateKey } =
-		destructureEnv(testEnvNetwork);
+export const testClientSession = async ({ network }: { network: Network }): Promise<Client> => {
+  // Retrieve parameters for session.
+  const { permission, actor, privateKey } = destructureEnv(network);
+  const chain = network;
 
-	if (!privateKey) {
-		throw new Error("Private key not found");
-	}
+  if (!privateKey) {
+    throw new Error("Private key not found");
+  }
 
-	const { url, id } = network;
+  // Create client
+  const client = createClient({ network });
 
-	// Create client
-	const client = createClient({ network });
+  // Setup wallet plugin with private key
+  const walletPlugin = new WalletPluginPrivateKey(privateKey);
 
-	// Set up wallet with privatekey
-	const pk: PrivateKeyType = PrivateKey.fromString(
-		privateKey.toString(),
-		false,
-	);
+  /** Create new permission level from representing types. Can be expressed as a string in the format `<actor>@<permission>`. */
+  const permissionLevel = PermissionLevel.from(`${actor}@${permission}`);
+  // Set up session with wallet and chain
+  const session = new Session({ actor, permission, walletPlugin, chain, permissionLevel });
 
-	const walletPlugin = new WalletPluginPrivateKey(pk);
+  // Connect session to client
+  await client.setSession(session);
 
-	// Set up session with wallet and chain
-	const session = new Session({
-		actor,
-		permission,
-		walletPlugin,
-		chain: { id, url },
-		permissionLevel: PermissionLevel.from(`${actor}@${permission}`),
-	});
-
-	// Connect session to client
-	await client.setSession(session);
-
-	return client;
+  return client;
 };
