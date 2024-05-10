951be62103259835dddd04df4a9321e27bcc05df
David Britt <david@effect.ai>
2024-05-10 12:38:50 -0400

    Fix getAvatar function signature with getAvatarArgs

--- src/actions/vaccount/getAvatar.ts
+++ src/actions/vaccount/getAvatar.ts
@@ -1,8 +1,10 @@
-import type { Client } from "../../client";
 import { getAsset } from "../atomic/getAsset";
-import { getAvatar as getDaoAvatar } from "../dao/getAvatar";
+import {
+	getAvatar as getDaoAvatar,
+	type GetAvatarArgs,
+} from "../dao/getAvatar";
 
-export const getAvatar = async (client: Client, account: string) => {
+export const getAvatar = async ({ client, account }: GetAvatarArgs) => {
 	const defaultImg = "QmZQiEWsaTNpANMv9orwDvuGyMRkY5nQNazSB1KkW4pM6t";
 	const defaultVid = "QmZQiEWsaTNpANMv9orwDvuGyMRkY5nQNazSB1KkW4pM6t";
 	const daoAvatar = await getDaoAvatar({ client, account });
