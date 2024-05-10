d297b42b40df02e3930cf1b9bcde07605dcaccf8
David Britt <david@effect.ai>
2024-05-10 16:18:15 -0400

    Add reserveTAsk.test.ts

---
+++ src/actions/tasks/reservations/reserveTask.test.ts
@@ -0,0 +1,15 @@
+import { expect, test, describe } from "bun:test";
+import { destructureEnv, testClientSession } from "../../../../test/src/utils";
+import { reserveTask } from "./reserveTask";
+
+describe("reserveTask", async () => {
+	// TODO: Figure out flow for how to test this function
+	test.todo("Should reserve task", async () => {
+		const client = await testClientSession();
+
+		const reservation = await reserveTask({ client, campaignId: 1 });
+		console.debug(reservation);
+
+		expect(reservation).toBeDefined();
+	});
+});
