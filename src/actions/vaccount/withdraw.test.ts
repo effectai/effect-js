734badbf32d61800e4f120b31516c18e2817d241
David Britt <david@effect.ai>
2024-05-10 13:48:14 -0400

    Add test to withdraw.test.ts

---
+++ src/actions/vaccount/withdraw.test.ts
@@ -0,0 +1,22 @@
+import { expect, test, describe } from "bun:test";
+import { destructureEnv, testClientSession } from "../../../test/src/utils";
+import { withdraw, jungle4 as network, createClient } from "../../exports";
+import type { ExtendedAssetType } from "@wharfkit/antelope";
+
+describe("withdraw", async () => {
+	test.skip("Should withdraw vaccount EFX to wallet.", async () => {
+		const client = await testClientSession();
+		const quantity: ExtendedAssetType = {
+			quantity: "0.0001 EFX",
+			contract: client.network.config.efx.contracts.token,
+		};
+		const result = await withdraw({ client, quantity });
+		console.log(result);
+		expect(result).toBeDefined();
+	});
+
+	test.todo("Should fail when no EFX is in vAccount", async () => {
+		// TODO: Should fail when no EFX is in the vAccount
+		// Figure out flow to test this
+	});
+});
