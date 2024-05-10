4c8f1cd7ad660db19f742faab68d6e09b3b436a5
David Britt <david@effect.ai>
2024-05-10 15:05:59 -0400

    Add getBatch.test.ts

---
+++ src/actions/tasks/batch/getBatch.test.ts
@@ -0,0 +1,32 @@
+import { expect, test, describe } from "bun:test";
+import { destructureEnv, testClientSession } from "../../../../test/src/utils";
+import {
+	createBatch,
+	getBatchById,
+	getCampaignById,
+	getIpfsResource,
+	jungle4 as network,
+} from "../../../exports";
+
+describe("getAvatar", async () => {
+	test("Should create a new batch for given campaign", async () => {
+		const campaignId = 1;
+		const client = await testClientSession();
+		const campaign = await getCampaignById({ client, id: campaignId });
+		const batchById = await getBatchById({
+			client,
+			id: campaign.active_batch,
+		});
+
+		const batch = await getBatchById({ client, id: batchById.id });
+		expect(batch).toBeDefined();
+		expect(batch.id).toBe(batchById.id);
+	});
+
+	test("Should return undefined if batch does not exist.", async () => {
+		const nonExistentBatchId = 999999999;
+		const client = await testClientSession();
+		const batch = await getBatchById({ client, id: nonExistentBatchId });
+		expect(batch).toBeUndefined();
+	});
+});
