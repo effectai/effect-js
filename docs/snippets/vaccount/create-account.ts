e6d0a6d9121758c979ef5f2d6de1ccbfd25858e1
David Britt <david@effect.ai>
2024-05-12 15:36:27 -0400

    Add code snippet for create account

---
+++ docs/snippets/vaccount/create-account.ts
@@ -0,0 +1,36 @@
+import { WalletPluginPrivateKey } from "@wharfkit/wallet-plugin-privatekey";
+import {
+	createClient,
+	jungle4 as chain,
+	// eos as chain,
+	Session,
+	createVAccount,
+	type CreateVAccountArgs,
+} from "@effectai/sdk";
+
+const actor = "actor-name";
+const permission = "permission-level";
+
+// Create a session
+const session = new Session({
+	chain,
+	actor,
+	permission,
+	walletPlugin: new WalletPluginPrivateKey("your_private_key"),
+});
+
+// Create client and connect session
+const client = await createClient({ session });
+
+const [vacc] = await getVAccounts({ client, actor });
+
+// Check pending payments
+const pendingPayments = await getPendingPayments({
+	client,
+	vAccountId: vacc.id,
+});
+
+// If there are claimable payments, claim them.
+if (pendingPayments.totalEfxClaimable > 0) {
+	await claim({ client });
+}
