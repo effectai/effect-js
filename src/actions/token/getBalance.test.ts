3ed66621872848d50bb6e16ac7281fe3f1ecd96e
David Britt <david@effect.ai>
2024-05-15 14:32:51 -0400

    Update getBalance.test.ts

--- src/actions/token/getBalance.test.ts
+++ src/actions/token/getBalance.test.ts
@@ -5,16 +5,20 @@ import { eos, jungle4 } from "../../exports";
 
 describe("getBalance", async () => {
 	test("getBalance() should retrieve balance from user on mainnet", async () => {
-		const client = await createClient({ network: jungle4 });
-		const actor = "forcedev1234";
+		const client = await createClient({ network: eos });
+		const actor = "cryptonode42";
 		const balance = await getBalance({ client, actor });
 		expect(balance).toBeDefined();
-		expect(balance.toString()).toBeDefined();
-		expect(balance.toString()).toContain("EFX");
+		expect(balance.efxBalance).toBeDefined();
+		expect(balance.usdtBalance).toBeDefined();
+		expect(balance.eosBalance).toBeDefined();
+		expect(balance.efxBalance).toBeGreaterThan(0);
+		expect(balance.usdtBalance).toBeGreaterThan(0);
+		expect(balance.eosBalance).toBeGreaterThan(0);
 	});
 
 	test("getBalance() should throw Error retrieving balance from unknown user.", async () => {
-		const client = await createClient({ network: eos });
+		const client = await createClient({ network: jungle4 });
 		const actor = "cryptonode99";
 		expect(async () => await getBalance({ client, actor })).toThrowError();
 	});
