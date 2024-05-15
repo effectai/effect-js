d11f11cdb4d3ad409667ab2a399befbc0292291a
Jeffrey <jeffrey@1nteger.nl>
2024-05-15 15:12:47 +0200

    Updates

---
+++ assets/introduction-DgyMMt68.js
@@ -0,0 +1,15 @@
+import{d as i,j as e}from"./index-BJTC2Cmi.js";const r={title:"Data Collection",description:"undefined"};function a(n){const t={a:"a",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",p:"p",strong:"strong",ul:"ul",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"data-collection",children:["Data Collection",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#data-collection",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsx(t.p,{children:`The primary use case of the Effect Network SDK revolves around leveraging its capabilities for data collection. In this section, we'll guide you through setting up your first data collection campaign.
+Whether you're building AI models, conducting surveys, or enhancing automated processes,
+the Effect Network offers a robust infrastructure to support your needs in a completely open, decentralized an transparent way.`}),`
+`,e.jsxs(t.h2,{id:"image-annotation-for-ai-training",children:["Image annotation for AI training",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#image-annotation-for-ai-training",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.p,{children:["For this tutorial, let's dive into a practical example: utilizing the Effect AI Network to annotate images distinguishing between a ",e.jsx(t.strong,{children:"chihuahua"})," and a ",e.jsx(t.strong,{children:"muffin"}),`.
+This annotated data will serve as training material for our AI model.
+It's important to note that this is just one of many potential applications. Other examples include:`]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Gathering survey data from a specific target audience"}),`
+`,e.jsx(t.li,{children:"Employing the Effect AI Network for final quality checks in a Language Model (LLM) pipeline for automated transcriptions"}),`
+`,e.jsx(t.li,{children:"Playing chess against a random human"}),`
+`,e.jsx(t.li,{children:"Many more!"}),`
+`]}),`
+`,e.jsxs(t.p,{children:["Please check out our ",e.jsx(t.a,{href:"https://github.com/effectai/effect-js/tree/dev/examples",children:"example folder"})," on github if you want to check out other examples or want to contribute!"]})]})}function s(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(a,{...n})}):a(n)}export{s as default,r as frontmatter};
