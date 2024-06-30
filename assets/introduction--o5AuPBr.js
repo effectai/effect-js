84dc33936697c1cebb7401f744d7c825431ff1ad
Jeffrieh <Jeffrieh@users.noreply.github.com>
2024-06-30 08:29:58 +0000

    deploy: c0a983fca7f465fc4a835399c07088cb8818a88d

---
+++ assets/introduction--o5AuPBr.js
@@ -0,0 +1,10 @@
+import{d as i,j as e}from"./index-CtGetNYQ.js";const r={title:"Why Effect AI",description:"undefined"};function a(t){const n={a:"a",div:"div",h1:"h1",h2:"h2",header:"header",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"why-effect-ai",children:["Why Effect AI",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#why-effect-ai",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.h2,{id:"problems",children:["Problems",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#problems",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:`AI will be the pivotal technology of the future that will transcend us into a new era of efficiency and innovation.
+However, the development of AI models is contingent upon the availability of high-quality training data.
+This poses a significant challenge, as the creation of such data is a labor-intensive and time-consuming process.
+Moreover, this data is often gathered & developed behind closed doors, leading to a lack of transparency and accountability.`}),`
+`,e.jsxs(n.h2,{id:"solution",children:["Solution",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#solution",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:`The solution is Effect AI, a decentralized network that connects AI developers with a global workforce of AI trainers.
+Leading the charge in the democratization of AI, Effect AI enables developers to create high-quality training data through a transparent and collaborative process.
+By leveraging the power of the crowd, Effect AI ensures that AI models are trained on diverse and reliable datasets, thereby enhancing their accuracy and performance.`})]})}function d(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(a,{...t})}):a(t)}export{d as default,r as frontmatter};
